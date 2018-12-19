c96cda124a47b0e437ab35c83614ff3d19b98ffa
Xiaoya <xiaoyaji1024@gmail.com>
2018-12-19 15:50:41 +0800

    Update mainwindow.cpp

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -1,16 +1,13 @@
 #include "gui/mainwindow.h"
-//#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent) /*,
-    ui(new Ui::MainWindow)*/
+    QMainWindow(parent)
 {
- //   ui->setupUi(this);
     gameBoard = new QGameBoard(this);
     setCentralWidget(gameBoard);
 }
 
 MainWindow::~MainWindow()
 {
-  //  delete ui;
+
 }
